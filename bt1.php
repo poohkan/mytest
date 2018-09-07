616233462475569199d663572f77fcb5596e61a4
poohkan <poohbah.w@gmail.com>
2018-09-07 19:35:32 +0700

    Add files via upload

---
+++ bt1.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link href="css/bootstrap.css" rel="stylesheet">
+<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
+
+    <title>Hello, Web!</title>
+  </head>
+  <body>
+    <div class="container-fluid">
+       <h1>Hello, WEB!</h1>
+    <div class="row">
+    <div class="span1">Column 1</div>
+    <div class="span1">Column 2</div>
+    <div class="span1">Column 3</div>
+    <div class="span1">Column 4</div>
+    <div class="span1">Column 5</div>
+    <div class="span1">Column 6</div>
+    <div class="span1">Column 7</div>
+    <div class="span1">Column 8</div>
+    <div class="span1">Column 9</div>
+    <div class="span1">Column 10</div>
+    <div class="span1">Column 11</div>
+    <div class="span1">Column 12</div>
+	</div>
+	<div class="row">
+    <div class="span8">Main</div>
+    <div class="span4">Sidebar</div>
+	</div>
+	<div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+<div class="alert alert-secondary" role="alert">
+  A simple secondary alert—check it out!
+</div>
+<div class="alert alert-success" role="alert">
+  A simple success alert—check it out!
+</div>
+<div class="alert alert-danger" role="alert">
+  A simple danger alert—check it out!
+</div>
+<div class="alert alert-warning" role="alert">
+  A simple warning alert—check it out!
+</div>
+<div class="alert alert-info" role="alert">
+  A simple info alert—check it out!
+</div>
+<div class="alert alert-light" role="alert">
+  A simple light alert—check it out!
+</div>
+<div class="alert alert-dark" role="alert">
+  A simple dark alert—check it out!
+</div>
+    </div>
+
+  	<script src="js/jquery-3.3.1.js"></script>
+	<script src="js/bootstrap.min.js"></script>  
+  </body>
+</html>
